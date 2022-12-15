d17d7dfac9feb0d330e178f84aeeec947c369bc2
hya <ilovecoding@foxmail.com>
2022-12-15 17:38:04 +0800

    edit build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path, { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+const timeStamp = new Date().getTime();
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   resolve: {
@@ -8,6 +9,12 @@ export default defineConfig({
       { find: '@', replacement: resolve(__dirname, 'src') },
       { find: '~bootstrap', replacement: resolve(__dirname, 'node_modules/bootstrap') },
     ],
+  },
+  build: {
+    outDir: path.resolve(__dirname, './dist'),
+    assetsDir: `${timeStamp}`,
+    chunkSizeWarningLimit: 2000,
+    sourcemap: false,
   },
   server: {
     port: 8080,
